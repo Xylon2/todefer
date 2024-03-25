d6df33e378e6b507b733908b10d05a54ef22a942
Xylon <joseph@xylon.me.uk>
2024-03-25 05:44:54 +0000

    missed this one out

---
+++ src/cljs/todefer/agenda.cljs
@@ -0,0 +1,15 @@
+(ns todefer.agenda
+  (:require [todefer.shared :refer [log byid byclass
+                                    setup-collapsibles
+                                    expand-it
+                                    toggle-element]]))
+
+;; want to expand the today section by default
+(let [elementname "today"
+      todaysection (.getElementById js/document elementname)]
+  (toggle-element elementname)
+  (expand-it todaysection))
+
+(setup-collapsibles)
+
+(.onLoad js/htmx setup-collapsibles)
